b76f3e8bc135ef58a538901a53a7b36edfdb7996
Hanson <hansonwu770@gmail.com>
2025-01-09 09:22:09 -0500

    TEST 2

--- src/Linker.js
+++ src/Linker.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes, useLocation, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./mainPage";
 import ToDoList from "./ToDoList";
 import Weather from "./Weather";
@@ -10,8 +11,8 @@ function Linker() {
     return (
             <Routes>
                 <Route path="/" element={<MainPage />} />
-                {/* <Route path="/ToDoList" element={<ToDoList />} /> */}
-                <Route path="/ToDoList" Component={ToDoList} />
+                <Route path="/ToDoList" element={<ToDoList />} />
+                {/* <Route path="/ToDoList" Component={ToDoList} /> */}
                 <Route path="/Weather" element={<Weather />} />
             </Routes>
     );
@@ -20,7 +21,8 @@ function Linker() {
 
 export default function Main() {
     return (
-        <Router basename="/Weather-and-Task-Management-Projects">
+        // <Router basename="/Weather-and-Task-Management-Projects">
+        <Router>
             <Linker />
         </Router>
     );
